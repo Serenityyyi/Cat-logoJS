8575e36a1ae9fa2561d0e33e5ff1825af4fa8d52
serenity <jorge4cuadros@gmail.com>
2024-11-17 15:38:50 +0100

    agrego todo el proyecto

---
+++ scritp.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+    $("#todo").click(function() {
+
+        $(".hideall").show();
+
+    });
+
+    $("#AA").click(function() {
+
+        $(".hideall").hide();
+        $("#A").show();
+
+    });
+
+    $("#BB").click(function() {
+
+        $(".hideall").hide();
+        $("#B").show();
+
+    });
+
+    $("#CC").click(function() {
+
+        $(".hideall").hide();
+        $("#C").show();
+
+    });
+
+    $("#DD").click(function() {
+
+        $(".hideall").hide();
+        $("#D").show();
+
+    });
+
+    $("#EE").click(function() {
+
+        $(".hideall").hide();
+        $("#E").show();
+
+    });
+
+    $("#FF").click(function() {
+
+        $(".hideall").hide();
+        $("#F").show();
+
+    });
+
+});
